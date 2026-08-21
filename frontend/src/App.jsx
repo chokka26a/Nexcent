a23ff665c26a4679ee18d5b33eafbf903a8a7a04
chokkadevops <chokka26a@gmail.com>
2026-08-21 18:20:33 +0530

    code changes adapt to design layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import './App.css';
 
 // Import local Figma section assets
 import logoImg from './assets/Iconimg.png';
+import heroImg from './assets/HeroSection.png'
 import clientsImg from './assets/Clients.png';
 import communityImg from './assets/Community.png';
 import achievementsImg from './assets/Achievements.png';
 import calendarImg from './assets/Calender.png';
 import customersImg from './assets/Customers.png';
 import communityUpdatesImg from './assets/CommunityUpdates.png';
+import bodyImg from './assets/Body.png';
+import footerImg from './assets/Footer.png';
 
 // Import AuthContext hook
 import { useAuth } from './context/AuthContext'; 
@@ -69,9 +72,10 @@ export default function App() {
 
       {/* 2. Page Content Layout */}
       <main>
+
         <section className="section-container bg-light">
-          <img src={achievementsImg} alt="Local Business Achievements" className="full-width-img" />
-        </section>
+          <img src={heroImg} alt="Hero banner" className="full-width-img" />
+        </section>  
 
         <section className="section-container">
           <img src={clientsImg} alt="Our Clients" className="full-width-img" />
@@ -81,6 +85,18 @@ export default function App() {
           <img src={communityImg} alt="Manage Community" className="full-width-img" />
         </section>
 
+          <section className="section-container">
+          <img src={bodyImg} alt="Main Body Layout" className="full-width-img" />
+        </section>
+
+          <section className="section-container">
+          <img src={footerImg} alt="Footer Layout" className="full-width-img" />
+        </section>
+
+    {/*     <section className="section-container bg-light">
+          <img src={achievementsImg} alt="Local Business Achievements" className="full-width-img" />
+        </section>
+        
         <section className="section-container">
           <img src={calendarImg} alt="Site Footer Design" className="full-width-img" />
         </section>
@@ -91,14 +107,14 @@ export default function App() {
 
         <section className="section-container">
           <img src={communityUpdatesImg} alt="Caring is the new marketing" className="full-width-img" />
-        </section>
+        </section> */}
       </main>
 
-      {/* 3. Footer Call to Action */}
-      <footer className="footer-cta">
+     {/* 3. Footer Call to Action */}
+     {/* <footer className="footer-cta">
         <h2>Pellentesque suscipit fringilla libero eu.</h2>
         <button className="btn-primary" onClick={() => setAuthMode('signup')}>Get a Demo →</button>
-      </footer> 
+      </footer>  */}
     
       {/* 4. Active Auth Modal Overlay */}
       {authMode && (
